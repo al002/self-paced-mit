8e90b84955c4afff4e56e883ba622357ed219f33
al <al002@disroot.org>
2019-11-28 11:29:03 +0800

    complete part4 of ps3

--- 6.0001/problem_sets/ps3/ps3.py
+++ 6.0001/problem_sets/ps3/ps3.py
@@ -16,7 +16,7 @@ CONSONANTS = 'bcdfghjklmnpqrstvwxyz'
 HAND_SIZE = 7
 
 SCRABBLE_LETTER_VALUES = {
-    'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1, 'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1, 's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10
+   '*': 0, 'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1, 'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1, 's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10
 }
 
 # -----------------------------------
@@ -204,7 +204,14 @@ def is_valid_word(word, hand, word_list):
     valid = False
     h = hand.copy()
 
-    if word.lower() in word_list:
+    if word.find('*') > -1:
+        for vowel in VOWELS:
+            if word.replace('*', vowel) in word_list:
+                valid = True
+    elif word.lower() in word_list:
+        valid = True
+
+    if valid:
         for letter in word:
             l = letter.lower()
             if h.get(l, 0) > 0:
